329978388d7d0901059b6cd56f5ab682ad29d6e3
정준영 <jyjeong620@naver.com>
2020-10-27 10:36:24 +0900

    TesCode 작성시 어떤 테스트인지 한눈에 확인하기위한 애노테이션을 적용 junit5 의경우 @Display 를 사용하면 편함

--- src/test/java/com/jyjeong/demoinfleanrestapi/events/EventControllerTest.java
+++ src/test/java/com/jyjeong/demoinfleanrestapi/events/EventControllerTest.java
@@ -1,6 +1,7 @@
 package com.jyjeong.demoinfleanrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jyjeong.demoinfleanrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +35,7 @@ public class EventControllerTest {
 //    EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("spring")
@@ -66,6 +68,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -94,6 +97,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -104,6 +108,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
